36ad80775e559db1a89763ad66126d25c9d4212b
sarden0302 <sarden030@gmail.com>
2025-02-20 14:23:47 +0900

    rider Query

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/mapper/RiderReviewMapper.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/mapper/RiderReviewMapper.java
@@ -2,8 +2,10 @@ package walking_beans.walking_beans_backend.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface RiderReviewMapper {
 
-    int[] getRiderReviewRatingAverage(int userId);
+    List<Integer> getRiderReviewRatingAverage(int riderId);
 }
