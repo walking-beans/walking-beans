97c4a6a3c43a859a56ff86b1e78df7e66496d32c
mochoping <hodolskm@gmail.com>
2025-02-20 12:09:25 +0900

    component 경로 추가

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/model/dto/Menu.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/model/dto/Menu.java
@@ -2,10 +2,24 @@ package walking_beans.walking_beans_backend.model.dto;
 
 import lombok.*;
 
+import java.sql.Timestamp;
+
 @ToString
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Menu {
+
+    private long menuId;
+    private long storeId;
+    private long userId;
+    private String menuPrice;
+    private String menuPictureUrl;
+    private String menuName;
+    private String menuCategory;
+    private String menuDescription;
+    private Timestamp menuCreateDate;
+    private Timestamp menuModifiedDate;
+
 }
--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/model/dto/Stores.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/model/dto/Stores.java
@@ -8,4 +8,22 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Stores {
+    private long storeId;
+    private long userId;
+    private String storeName;
+    private String storeDescription;
+    private String storeMainMenu;
+    private long storeBusinessNumber;
+    private String storePhone;
+    private String storePictureUrl;
+    private String storeOperationHours;
+    private String storeClosedDates;
+    private String storeStatus;
+    private int storeReviewCount;
+    private String storeRating;
+    private short storeMinDeliveryTime;
+    private short storeMaxDeliveryTime;
+    private int storeDeliveryTip;
+    private String storeDeliveryAddress; // storeDeliveryArea 로 변경요청
+    private String storeAddress;
 }
