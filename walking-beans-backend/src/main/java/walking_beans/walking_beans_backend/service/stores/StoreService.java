eb59e4f6a75bc2ea50f9b7db22ad9606509b15c0
mochoping <hodolskm@gmail.com>
2025-02-21 16:32:48 +0900

    store, menu, options mappers 완료

---
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/service/stores/StoreService.java
@@ -0,0 +1,29 @@
+package walking_beans.walking_beans_backend.service.stores;
+
+import walking_beans.walking_beans_backend.model.dto.Stores;
+
+import java.util.List;
+
+public interface StoreService {
+
+    //매장 전체 불러오기
+    List<Stores> findAllStores();
+
+    //매장 검색
+    List<Stores> searchStores();
+
+    //특정 매장 불러오기, ID
+    Stores findStoresById(long storeId);
+
+    // 회원(업주) ID로 매장 불러오기
+    Stores findStoresByuserId(long userId);
+
+    // 신규매장 등록하기
+    void addStores(Stores stores);
+
+    // 매장정보 수정하기
+    void updateStores(Stores stores);
+
+    // 매장정보 삭제하기
+    void deleteStores(long storeId);
+}
