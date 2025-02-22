7e75695ef945035ecc59d5b6c722b9e26e1bd2d8
sarden0302 <sarden030@gmail.com>
2025-02-23 07:55:42 +0900

    0223

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/controller/AddressAPIController.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/controller/AddressAPIController.java
@@ -1,15 +1,18 @@
 package walking_beans.walking_beans_backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import walking_beans.walking_beans_backend.model.dto.Address;
 import walking_beans.walking_beans_backend.service.addressService.AddressService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/addresses")
 public class AddressAPIController {
@@ -21,5 +24,17 @@ public class AddressAPIController {
     public ResponseEntity<List<Address>> getAllAddresses() {
         List<Address> addresses = addressService.getAllAddresses();
         return ResponseEntity.ok(addresses);
+    }
+
+
+
+
+    /********** LEO **********/
+    // 유저 대표 주소 가져오기 by order_id
+    @GetMapping("/userMainByOrderId")
+    public ResponseEntity<Address> getUserMainAddress(@RequestParam("orderId") long orderId,
+                                                      @RequestParam("userId") long userId){
+        log.info("=== /api/addresses/userMainByOrderId?orderId={}&userId={} ===", orderId, userId);
+        return ResponseEntity.ok(addressService.getUserMainAddress(orderId, userId));
     }
 }
--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/mapper/AddressMapper.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/mapper/AddressMapper.java
@@ -9,4 +9,9 @@ import java.util.List;
 public interface AddressMapper {
     // 모든 주소 조회
     List<Address> getAllAddresses();
+
+
+    /********** LEO **********/
+    // 유저 대표 주소 가져오기 by order_id
+    Address getUserMainAddress(long orderId, long userId);
 }
