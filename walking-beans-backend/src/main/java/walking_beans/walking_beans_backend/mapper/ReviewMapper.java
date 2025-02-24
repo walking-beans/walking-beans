59ba5ad64512a2d7de35ddfee62c8e0c421fb844
zhwoddl1001 <zhwoddl0723@gmail.com>
2025-02-24 17:43:29 +0900

    address 백엔드 review 백엔드 insertAddress 리액트

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/mapper/ReviewMapper.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/mapper/ReviewMapper.java
@@ -1,7 +1,21 @@
 package walking_beans.walking_beans_backend.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import walking_beans.walking_beans_backend.model.dto.Reviews;
+
+import java.util.List;
 
 @Mapper
 public interface ReviewMapper {
+    // 특정 매장의 리뷰 조회
+    List<Reviews> getReviewsByStore(long storeId);
+
+    // 리뷰 추가
+    void insertReview(Reviews review);
+
+    // 리뷰 수정
+    void updateReview(Reviews review);
+
+    // 리뷰 삭제
+    void deleteReview(long reviewId);
 }
