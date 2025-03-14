cc4d9f46ccf5a2429dc81fe6ebc00f8103ce18c9
zhwoddl1001 <zhwoddl0723@gmail.com>
2025-03-14 17:48:53 +0900

    리뷰 추가 이미지추가

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/config/SecurityConfig.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+
     //비밀번호 암호화 처리하는 자바기능 불러와서
     // Bean 설정하여 스프링부트 자체에서 사용자가 작성한 비밀번호를
     // 암호화 처리하여 저장할 수 있도록 설정
@@ -21,6 +23,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin(form -> form.disable());
+
         return http.build(); // build = 빌딩  위에서 작성한 http 속성으로 뭉쳐놓은 다음 사용
     }
 }
