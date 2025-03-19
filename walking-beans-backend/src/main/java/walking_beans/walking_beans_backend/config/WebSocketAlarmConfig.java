d374d0f8caa9682ca0de193b67a351532af1c1e4
smdh1458 <smdh1458@hanmail.net>
2025-03-19 16:30:30 +0900

    알람 웹소켓 config파일 이름 변경

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/config/WebSocketAlarmConfig.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/config/WebSocketAlarmConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketAlarmConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
