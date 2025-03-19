92ffd36e30209cac8810174d14f49a32ab0dd957
sarden0302 <sarden030@gmail.com>
2025-03-19 21:46:16 +0900

    back to http

--- walking-beans-frontend/src/pages/admin/AdminMessage.jsx
+++ walking-beans-frontend/src/pages/admin/AdminMessage.jsx
@@ -120,7 +120,7 @@ import PictureButton from "../../assert/images/admin/AdminMessage/adminMessage-p
 import SendAlarm from "../../components/admin/SendAlarm";
 
 
-function AdminMessage({user}) {
+const AdminMessage = ({user}) => {
 
     const {roomId} = useParams();
     const stompClient = useRef(null);
@@ -205,10 +205,9 @@ function AdminMessage({user}) {
             };
             stompClient.current.send(`/pub/message`, {}, JSON.stringify(body));
             stompClient.current.send(`/pub/chatting`, {}, JSON.stringify(body));
-            setSendAlarmMessage(inputValue); //알람용 텍스트 내용 저장
+            // setSendAlarmMessage(inputValue); //알람용 텍스트 내용 저장
             setInputValue('');
-            setShowSendAlarm(true); //알람 전송 트리거
-            return;
+            // setShowSendAlarm(true); //알람 전송 트리거
         } else if (stompClient.current && previewImage) {
             /*const reader = new FileReader();
             reader.readAsDataURL(previewImage);
@@ -228,9 +227,8 @@ function AdminMessage({user}) {
             setIsDisabled(false);
         } else {
             alert("메세지를 작성해주세요");
-            setSendAlarmMessage("이미지가 전송되었습니다."); //알림 내용 저장
-            setShowSendAlarm(true); //알림 전송 트리거
-            return;
+            // setSendAlarmMessage("이미지가 전송되었습니다."); //알림 내용 저장
+            // setShowSendAlarm(true); //알림 전송 트리거
         }
         // 알람 컴포넌트
 
