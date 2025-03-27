95e92d54abe6aee2e545c11a5f7471ed151a0eba
Leo <sarden030@gmail.com>
2025-03-27 22:51:32 +0900

    test

--- walking-beans-frontend/src/pages/rider/RiderIncome.jsx
+++ walking-beans-frontend/src/pages/rider/RiderIncome.jsx
@@ -3,6 +3,7 @@ import apiRiderService from "../../service/apiRiderService";
 import "../../css/rider/RiderIncome.css";
 
 
+
 const RiderIncome = ({user}) => {
 //테스트
     const [incomeList, setIncomeList] = useState([]);
