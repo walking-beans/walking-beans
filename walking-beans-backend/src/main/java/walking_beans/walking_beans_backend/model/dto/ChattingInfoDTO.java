81bc3db3aad77bb5feb9ee3af4e676e70ad8270e
Leo <sarden030@gmail.com>
2025-04-04 09:57:52 +0900

    Chattingroom 시간 설정

--- walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/model/dto/ChattingInfoDTO.java
+++ walking-beans-backend/src/main/java/walking_beans/walking_beans_backend/model/dto/ChattingInfoDTO.java
@@ -1,10 +1,13 @@
 package walking_beans.walking_beans_backend.model.dto;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Data
 public class ChattingInfoDTO {
     // 채팅방 정보
@@ -27,40 +30,16 @@ public class ChattingInfoDTO {
     private String receiverPictureUrl;
 
     public void setTimeDifference() {
-       /* this.orderCreatedDay = orderCreateDate.getMonthValue() + "/" + orderCreateDate.getDayOfMonth();
-        this.orderCreatedTime = (orderCreateDate.getHour() < 10 ? "0" + orderCreateDate.getHour() : orderCreateDate.getHour()) + ":" + (orderCreateDate.getMinute() < 10 ? "0" + orderCreateDate.getMinute() : orderCreateDate.getMinute());
-        this.orderDeliveredDay = orderModifiedDate.getMonthValue() + "/" + orderModifiedDate.getDayOfMonth();
-        this.orderDeliveredTime = (orderModifiedDate.getHour() < 10 ? "0" + orderModifiedDate.getHour() : orderModifiedDate.getHour())  + ":" + (orderModifiedDate.getMinute() < 10 ? "0" + orderModifiedDate.getMinute() : orderModifiedDate.getMinute());
-        if (orderCreatedDay.equals(orderDeliveredDay)) {
-            this.orderDeliveredDay = null;
-        }*/
+       Duration duration = Duration.between(modifiedDate, LocalDateTime.now());
+       long ofDays = duration.toDays();
+       log.info("Time difference: " + duration.toMinutes() + " minutes");
+       if (ofDays > 364) {
+           this.timeDifference = String.valueOf(ofDays / 365) + "년 전";
+           return;
+       }
 
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        if (currentDateTime.getYear() < modifiedDate.getYear()) {
-            this.timeDifference = modifiedDate.minusYears(currentDateTime.getYear()).getYear() + "년 전";
-            return;
-        }
-
-        if (currentDateTime.getMonthValue() < modifiedDate.getMonthValue()) {
-            this.timeDifference = modifiedDate.minusMonths(currentDateTime.getMonthValue()).getMonthValue() + "달 전";
-            return;
-        }
-
-        if (currentDateTime.getDayOfMonth() < modifiedDate.getDayOfMonth()) {
-            this.timeDifference = modifiedDate.minusDays(currentDateTime.getDayOfMonth()).getDayOfMonth() + "일 전";
-            return;
-        }
-
-        if (currentDateTime.getHour() < modifiedDate.getHour()) {
-            this.timeDifference = modifiedDate.minusHours(currentDateTime.getHour()).getHour() + "시간 전";
-            return;
-        }
-
-        if (currentDateTime.getMinute() < modifiedDate.getMinute()) {
-            this.timeDifference = modifiedDate.minusMinutes(currentDateTime.getMinute()).getMinute() + "분 전";
-            return;
-        }
-
-        this.timeDifference = "방금 전";
+       if (ofDays > 30) {
+           this.timeDifference = String.valueOf((ofDays / 30)) + "달 전";
+       }
     }
 }
